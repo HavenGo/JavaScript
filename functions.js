575cf8d0939480b4fb7b997da8da3dd4dafd8bcd
HavenGo <havengoitom2@gmail.com>
2024-08-27 14:15:19 +0300

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,18 @@
+function findSum(x, y) {
+    const answer = x + y;
+    return answer;
+}
+const sum = findSum(99, 1000)
+console.log(sum)
+// another function 
+function laugh() {
+    const result = 'hahahahahahahahahaha!';
+    return result;
+}
+console.log(laugh())
+/* another function */
+function gigle(num) {
+    const output = 'ha'.repeat(num) + '!';
+    return output;
+}
+console.log(gigle(3));
